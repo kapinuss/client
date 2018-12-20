e39f6d06bdd29ca9603bffe280b8826356b55e5c
kapinuss <stanislav.kapinus@gmail.com>
2018-12-20 15:31:11 +0300

    client-thursday

--- client.go
+++ client.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
-
-	"github.com/jasonlvhit/gocron"
+	"time"
 )
 
 func main() {
 
-	gocron.Every(1).Second().Do(innerFunc("user"))
+	/*
+		gocron.Every(1).Second().Do(innerFunc("user"))
+		gocron.Clear()
+		<-gocron.Start()
+	*/
 
-	gocron.Clear()
+	var numbers = randomArray(10, 10)
+	fmt.Println(len(numbers))
 
-	<-gocron.Start()
+	var x = string(6)
+	var y json.RawMessage
+	y, _ = json.Marshal(x)
+	fmt.Println(string(y))
+
+	serialize(1, numbers)
 
 }
 
@@ -23,4 +34,33 @@ func innerFunc(name string) string {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hello, world!")
+}
+
+func randomNums(num int) (int, int) {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(num), rand.Intn(num)
+}
+
+func randomArray(size int, top int) []int {
+	rand.Seed(time.Now().Unix())
+	var result = make([]int, size)
+	for i := 0; i < 10; i++ {
+		result[i] = rand.Intn(top)
+	}
+	return result
+}
+
+func serialize(number int, numbers []int) {
+	type Data struct {
+		id   int
+		nums []int
+	}
+
+	entity := Data{id: number, nums: numbers}
+
+	b, error := json.Marshal(entity)
+	if error != nil {
+		fmt.Println("Error: ", error)
+	}
+	fmt.Println(b)
 }
